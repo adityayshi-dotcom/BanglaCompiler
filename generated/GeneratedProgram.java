5847fdc5ee91f280127b2ec6f0fefce894f18d38
Adity Das Ayshi <adityayshi@gmail.com>
2026-08-28 14:38:08 -0700

    Complete Bangla compiler integration

---
+++ generated/GeneratedProgram.java
@@ -0,0 +1,16 @@
+public class GeneratedProgram {
+    public static void main(String[] args) {
+        int বয়স = 20;
+        int ফল = (বয়স + (5 * 2));
+        String অবস্থা = "শিশু";
+        if ((বয়স >= 18)) {
+            অবস্থা = "প্রাপ্তবয়স্ক";
+        }
+        else {
+            অবস্থা = "শিশু";
+        }
+        while ((বয়স < 25)) {
+            বয়স = (বয়স + 1);
+        }
+    }
+}
